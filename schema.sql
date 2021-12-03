ca8025e988d8adcb9a2cc19c88a4bf982a32315c
JED FLUXMAN <jedfluxman@JEDs-MacBook-Pro.local>
2021-12-02 16:50:26 -0800

    file name change

--- schema.sql
+++ schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -36,3 +36,21 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+select * from titles

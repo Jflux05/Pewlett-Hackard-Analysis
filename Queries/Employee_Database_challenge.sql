68e388340ee6806eec1964186d15032a4462da47
JED FLUXMAN <jedfluxman@JEDs-MacBook-Pro.local>
2021-12-12 16:44:07 -0800

    mods to query file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -7,10 +7,10 @@ SELECT e.emp_no,
 				t.to_date
 INTO retirement_titles
 FROM employees AS e
-INNER JOIN titles AS t
-    ON (e.emp_no = t.emp_no)
-INNER JOIN dept_emp AS de
-    ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+   		ON (e.emp_no = t.emp_no)
+			INNER JOIN dept_emp AS de
+    			ON (e.emp_no = de.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
     AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
@@ -56,10 +56,10 @@ SELECT * FROM mentorship_eligibility
 SELECT d.dept_name, COUNT(me.emp_no)
 INTO ment__dept_candidate_count
 FROM mentorship_eligibility as me
-INNER JOIN dept_emp as de
-	ON (me.emp_no = de.emp_no)
-INNER JOIN departments as d
-	ON (de.dept_no = d.dept_no)
+	INNER JOIN dept_emp as de
+		ON (me.emp_no = de.emp_no)
+			INNER JOIN departments as d
+				ON (de.dept_no = d.dept_no)
 GROUP BY d.dept_name
 ORDER BY COUNT(me.emp_no) DESC;
 
